7119beda7946ee42f66e272e624c5416868ffd40
DongMyeongLee <dongmyeong.lee22@gmail.com>
2019-12-25 03:51:16 +0900

    바이트 코드 조작하기, javaagent를 이용하여 다른 프로젝트에서 적용

--- TheJava/src/main/java/me/sun/Masul.java
+++ TheJava/src/main/java/me/sun/Masul.java
@@ -1,16 +1,36 @@
 package me.sun;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.ClassFileLocator;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
+
+import java.io.File;
 import java.io.IOException;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Masul {
     public static void main(String[] args) throws IOException {
         /* 바이트 코드 조작
         .class보면 바이트코드가 변경된것을 알 수 있다.
+
          */
+
+        // 이렇게 classloader를 이용하면 ByteBuddy에서 Moja를 호출하지 않아 출력을 동시에 해도 적용 된다.
+//        ClassLoader classLoader = Masul.class.getClassLoader();
+//        TypePool typePool = TypePool.Default.of(classLoader);
+//        new ByteBuddy().redefine(
+//                typePool.describe("me.sun.Moja").resolve(),
+//                ClassFileLocator.ForClassLoader.of(classLoader)
+//        )
+//                .method(named("pullOut")).intercept(FixedValue.value("Rabbit!!"))
+//                .make().saveIn(new File("C:\\Users\\dmdsj\\MyProject\\javalanguage\\TheJava\\target\\classes"));
+
 //        new ByteBuddy().redefine(Moja.class)
 //                .method(named("pullOut")).intercept(FixedValue.value("Rabbit!!"))
 //                .make().saveIn(new File("C:\\Users\\dmdsj\\MyProject\\javalanguage\\TheJava\\target\\classes"));
-//
+
         System.out.println(new Moja().pullOut());
     }
 }
