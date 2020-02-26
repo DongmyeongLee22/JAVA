7c45d06a4889da3205b695214b94f5dbf92db4d6
DongmyeongLee <dongmyeong.lee22@gmail.com>
2020-02-26 20:20:13 +0900

    WhiteShip Spring Boot ::: 외부 설정(간단히)
    프로젝트하면서 다 경험했던거라 크게 정리안함

--- SpringBoot-Inflearn/Spring-Boot-EX_1/src/test/java/me/sun/springbootex1/SpringBootEx1ApplicationTests.java
+++ SpringBoot-Inflearn/Spring-Boot-EX_1/src/test/java/me/sun/springbootex1/SpringBootEx1ApplicationTests.java
@@ -1,13 +1,33 @@
 package me.sun.springbootex1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SpringBootEx1ApplicationTests {
 
+    @Autowired
+    Environment environment;
+
     @Test
     void contextLoads() {
+    }
+
+    /**
+     * test.resources에 properties만들면 그 properties가 적용됨.
+     * - main 컴파일 후 test 컴파일할 때 그 properties가 덮어써진다.
+     * - main properties에 있는데 값이 test properties가 없으면 테스트 시 에러 발생(만약 메인에서 해당 값을 쓰면 부트 돌리면서 에러)
+     * - 우선순위는 따로 자료를 참고하자
+     * - 최상위에 config 폴더를 만들어서 거기에 설정하면 가장 우선순위가 높다.
+     */
+    @Test
+    void propertyTests() throws Exception {
+        assertThat(environment.getProperty("dexter.name"))
+                .isEqualTo("dexter");
     }
 
 }
